e47c3c899a461bd14a22003febaa40451fc1569f
piavw <piavonwachenfelt@hotmail.com>
2019-08-29 14:35:17 +0200

    Tidied up the code in the My_Group challange - mygroup.rb and created README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+This repository has the exercises with Ruby for the ITP-course of august 2019 with Craft Academy. 
+The file mygroup.rb is for the My_group challenge. 
--- mygroup.rb
+++ mygroup.rb
@@ -1,15 +1,6 @@
-=begin
-persons = ['Thomas is a 40 year old male.', 'Kajsa is a 30 year old woman,', 'Anders is a 33 year old male.']
-persons.each do |person| 
-  puts "#{person}"
-end
-:) This is a cheat. 
-=end 
-
-#This is borrowed :) 
-person_1 = {name: "Augustus", age: 77, gender: "male"}
-person_2 = {name: "Daisy", age: 88, gender: "female"}
-person_3 = {name: "Kitten", age: 99, gender: "possibly female?"}
+person_1 = {name: "Carl", age: 77, gender: "male"}
+person_2 = {name: "Agnes", age: 88, gender: "female"}
+person_3 = {name: "Sofie", age: 99, gender: "female"}
 
 my_group = [person_1, person_2, person_3]
 
